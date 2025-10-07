61135875135097d2cd23cc21d172637de76cfcc7
ROIFHAKIMF <Rhfgamingyt@gmail.com>
2025-10-07 07:45:11 +0700

    Install dan coba LiveWire

---
+++ app/Livewire/Counter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+        // Nah ini dia variabelnya
+    public $count = 0;
+
+    // Fungsi buat nambah angka
+    public function increment()
+    {
+        $this->count++;
+    }
+
+    public function decrement()
+    {
+        $this->count--;
+    }
+    public function render()
+    {
+        return view('livewire.counter');
+    }
+}
