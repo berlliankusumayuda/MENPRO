296fec44d2c8a4d76f4301c6ac0130b4da1302bb
rafiakbar07 <rafiakbarhakim04@gmail.com>
2025-10-13 16:18:54 +0700

    html login page

--- resources/views/livewire/login-page.blade.php
+++ resources/views/livewire/login-page.blade.php
@@ -1,3 +1,46 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+        <div class="body_container">
+            <div calss="logo_container">
+                <img src="" alt="" class="logo_img">
+                <div calss="logo_text">
+                    <h3>GERGAJI</h3>
+                    <p>Data Warga RT</p>
+                </div>
+            </div>
+            <div class="login_header">
+                <h1>Login</h1>
+                <div class="line"></div>
+            </div>
+           <from class="login_form">
+                <div class="input_group">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" placeholder="Masukkan Username">
+                </div>
+                <div class="input_group">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" placeholder="Masukkan Password">
+                </div>
+                <p>lupa kata sandi? <a href="">klik disini</a></p>
+                <button type="submit" class="login_button">Login</button>
+              </from>
+              <div class="atau">
+                <div class="gariskiri"></div>
+                <p>atau</p>
+                <div class="gariskanam"></div>
+              </div>
+                <div class="register_link">
+                    <p>Belum punya akun? <a href="">Daftar disini</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
