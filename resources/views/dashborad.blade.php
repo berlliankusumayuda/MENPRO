eb9a8c13cdf759a740e535b4ccc97f882bd83353
rafiakbar07 <rafiakbarhakim04@gmail.com>
2025-10-17 13:31:31 +0700

    sidebar

--- resources/views/dashborad.blade.php
+++ resources/views/dashborad.blade.php
@@ -4,12 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel</title>
-
+     @vite(['resources/css/sidebar.css'])
     @livewireStyles
 </head>
 <body>
-    <h1>Halo Laravel 👋</h1>
-    <livewire:counter />
+    <livewire:sidebar />
 
     @livewireScripts
 </body>
--- resources/views/livewire/sidebar.blade.php
+++ resources/views/livewire/sidebar.blade.php
@@ -1,3 +1,31 @@
-<div>
-    {{-- The whole world belongs to you. --}}
+<div class="sidebar">
+    <div class="sidebar_header">
+        <input type="checkbox" id="checkbox">
+        <label for="checkbox" class="toggle">
+        <div class="bar bar--top"></div>
+        <div class="bar bar--middle"></div>
+        <div class="bar bar--bottom"></div>
+        </label>
+        <h2>Gergaji</h2>
+    </div>
+   <div class="sidebar-content">
+    <div class="beranda">
+        <a href="">Beranda</a>
+    </div>
+    <div class="tabel-warga">
+        <a href="">Tabel Warga</a>
+    </div>
+    <div class="data-warga">
+        <a href="">Data Warga</a>
+    </div>
+    <div class="biodata">
+        <a href="">Biodata</a>
+    </div>
+    <div class="grafik">
+        <a href="">Grafik</a>
+    </div>
+    <div class="logout">
+        <a href="">logout</a>
+    </div>
+</div>
 </div>
